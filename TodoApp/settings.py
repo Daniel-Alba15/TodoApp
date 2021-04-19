39efec58cbd8c03d3944b22c4b623e293d672348
Daniel <danielorlandoa8@gmail.com>
2021-04-19 04:55:18 -0500

    .

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = 'TodoApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'todoapp',
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':BASE_DIR / 'db.sqlite3',
     }
 }
 
